b0ef66d74cccc4d26897b2b1443f74797ba9ce2c
Daniel Mora Rodriguez <dmorarod16@gmail.com>
2024-09-10 18:11:08 -0600

    Added backend project of the lab 5

---
+++ Lab5/backend-lab/Controllers/CountriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola Mundo";
+        }
+    }
+}
